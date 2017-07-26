784d9dae422e16048a9e6d62a13e20c45f2e3d25
vovgre <vovgre@ya.ru>
2017-07-26 21:10:52 +0300

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
     'serverPort':5000,
-    'dbUrl':'mongodb://<vovgre:<vovgre>@ds123933.mlab.com:23933/imgupload',
+    'dbUrl':'mongodb://<vovgre>:<vovgre>@ds123933.mlab.com:23933/imgupload',
     'cloud_name': 'dri4jrgkf',
     'api_key': '144571544352698',
     'api_secret': 'F7TmkfVmtK77zGwRPK_-WBqOMFI'
