5f4bf6b746263f39d988a8ac27b7d269f0a0b972
vovgre <vovgre@ya.ru>
2017-07-26 20:56:12 +0300

    Update route.js

--- route/route.js
+++ route/route.js
@@ -6,7 +6,7 @@ module.exports = function(express,app){
 	var router = express.Router();
 
 	router.get('/',function(req,res){
-		res.json("API Home Page");
+		res.json("Shisha app API Home Page!");
 	})
 	router.post('/api/v1/post/upload', multipart, postCtrl.upload);
     router.get('/api/v1/posts',postCtrl.get);
